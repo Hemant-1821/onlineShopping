884c1ce1006fced16acd008a0984110919666258
Hemant Singh <singh.hemant9583@gmail.com>
2020-05-05 17:25:58 +0530

    final commit with some major updates: Cart,Add Products,Remove Products,Edit Products

--- controllers/error.js
+++ controllers/error.js
@@ -1,4 +1,4 @@
 exports.notFound = (req,res,next)=>{
-    res.render('404',{docTitle:'Page Not Found',path:'Error'});
+    res.render('404',{pageTitle:'Page Not Found',path:'Error'});
     //res.status(404).sendFile(path.join(__dirname,'views','404.html'));
 };
